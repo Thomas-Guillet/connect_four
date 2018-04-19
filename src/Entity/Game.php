ffdac76a02b31e364817d2c469e3405d64013e5f
Unknown <thomas.guillet35@hotmail.fr>
2018-04-19 19:08:50 +0200

    WIP
    TODO: relations in database

---
+++ src/Entity/Game.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
+ */
+class Game
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var Owner
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="Game", cascade={"persist"})
+     * @ORM\Column(type="integer")
+     */
+    private $owner;
+
+    /**
+     * @var Player
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="Game", cascade={"persist"})
+     * @ORM\Column(type="integer")
+     */
+    private $player;
+
+    /**
+     * @ORM\Column(type="string", length=8)
+     */
+    private $state;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $pieces;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=7)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getOwner(): ?int
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(int $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    public function getPlayer(): ?int
+    {
+        return $this->player;
+    }
+
+    public function setPlayer(?int $player): self
+    {
+        $this->player = $player;
+
+        return $this;
+    }
+
+    public function getState(): ?string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): self
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    public function getPieces(): ?string
+    {
+        return $this->pieces;
+    }
+
+    public function setPieces(string $pieces): self
+    {
+        $this->pieces = $pieces;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20180419160912.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180419160912 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE game (id INT AUTO_INCREMENT NOT NULL, owner INT NOT NULL, player INT DEFAULT NULL, state VARCHAR(8) NOT NULL, pieces LONGTEXT NOT NULL, code VARCHAR(255) NOT NULL, type VARCHAR(7) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE game');
+    }
+}
